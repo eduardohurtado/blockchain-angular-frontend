875dbcf6145e9231828e8344db3a66a1892f3ceb
Eduardo Hurtado <lehurtadog@outlook.com>
2025-07-31 09:13:36 -0500

    Feature(003): - Application language now is persistent. - Navbar code improvements. - Added needed app component pages. - Added enums and languages keys.

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsaSvgFlagComponent } from '../languages-svg-flags/usa-svg-flag/usa-svg-flag.component';
 import { DeutschSvgFlagComponent } from '../languages-svg-flags/deutsch-svg-flag/deutsch-svg-flag.component';
 import { ItalianoSvgFlagComponent } from '../languages-svg-flags/italiano-svg-flag/italiano-svg-flag.component';
 import { ChineseSvgFlagComponent } from '../languages-svg-flags/chinese-svg-flag/chinese-svg-flag.component';
 import { EspanolSvgFlagComponent } from '../languages-svg-flags/espanol-svg-flag/espanol-svg-flag.component';
-import { APP_CODE_LANGUAGES, APP_LANGUAGES } from 'src/app/enums/emuns';
+import { APP_CODE_LANGUAGES, APP_LANGUAGES, LOCAL_STORAGE_KEYS } from 'src/app/enums/emuns';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ToastNotificationSignalService } from 'src/app/services/toast-notification.signal.service';
 
@@ -23,17 +24,25 @@ import { ToastNotificationSignalService } from 'src/app/services/toast-notificat
         EspanolSvgFlagComponent
     ]
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
     @ViewChild('language-dropdown-menu') dropdownRef!: ElementRef;
 
     appLanguages = APP_LANGUAGES;
     isLanguageDropdownOpen = false;
     selectedLanguage = APP_LANGUAGES.ENGLISH;
+    selectedLanguageCode = APP_CODE_LANGUAGES.ENGLISH;
 
     constructor(
         private translate: TranslateService,
+        private router: Router,
         private toastNotificationSignalService: ToastNotificationSignalService
     ) {}
+
+    ngOnInit(): void {
+        const storedLang = localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE) || APP_CODE_LANGUAGES.ENGLISH;
+        const key = Object.entries(APP_CODE_LANGUAGES).find(([_, v]) => v === storedLang)?.[0];
+        this.selectedLanguage = APP_LANGUAGES[key as keyof typeof APP_LANGUAGES] || APP_LANGUAGES.ENGLISH;
+    }
 
     toggleLanguageDropdown() {
         this.isLanguageDropdownOpen = !this.isLanguageDropdownOpen;
@@ -46,6 +55,7 @@ export class NavBarComponent {
             'en';
         const languageCode = APP_CODE_LANGUAGES[languageKey as keyof typeof APP_CODE_LANGUAGES] || 'en';
         this.translate.use(languageCode);
+        localStorage.setItem(LOCAL_STORAGE_KEYS.LANGUAGE, languageCode);
     }
 
     closeLanguageDropdown() {
@@ -54,6 +64,10 @@ export class NavBarComponent {
 
     triggerToastNotificationSignalService() {
         this.toastNotificationSignalService.triggerToastNotification();
+    }
+
+    isActive(path: string): boolean {
+        return this.router.url === path;
     }
 
     @HostListener('document:click', ['$event'])
